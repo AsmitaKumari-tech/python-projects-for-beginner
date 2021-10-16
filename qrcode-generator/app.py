85a537a3707681ccb27a3321bbf52eaa0a891962
unknown <kr.aman1@outlook.com>
2021-10-16 19:12:26 +0530

    Added Decode feature

--- qrcode-generator/app.py
+++ qrcode-generator/app.py
@@ -3,14 +3,24 @@ import qrcode
 import datetime
 repeat = True
 while(repeat):
-    x = str(datetime.datetime.now())
-    x = x.replace(':', "").replace('-', "").replace(' ', "").replace('.', "")
-    data = input("Enter the url: ")
-    img = qrcode.make(data)
-    pathname = "QR"+x
-    img.save(pathname + ".png")
+    task = input("What you want to do? (Encode or Decore a QR Code)")
+    if (task == 'Encode' or task == 'encode'):
+        x = str(datetime.datetime.now())
+        x = x.replace(':', "").replace('-', "").replace(' ', "").replace('.', "")
+        data = input("Enter the url: ")
+        img = qrcode.make(data)
+        pathname = "QR"+x
+        img.save(pathname + ".png")
+    else:
+        filename = input("Enter the filname of QR code image(with extension): ")
+        import cv2
+        d = cv2.QRCodeDetector()
+        val, _, _ = d.detectAndDecode(cv2.imread(filename))
+        print("Decoded text is: ", val)
+
     rinput = input("Do you want to repeat it: ")
     if(rinput == "y" or rinput == "yes" or rinput == "Y" or rinput == "YES"):
         repeat = True
     else:
         repeat = False
+
